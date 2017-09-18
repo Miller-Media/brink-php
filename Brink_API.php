7098bd388821304d89cef6fd5bd2b8d5ac59854d
mm-max <30533836+mm-max@users.noreply.github.com>
2017-09-18 16:30:51 -0700

    Add files via upload

---
+++ Brink_API.php
@@ -0,0 +1,187 @@
+<?php
+
+class Brink_API {
+	
+	public $access_token;
+	public $user_id;
+	private $api_baseurl;
+	
+	public function __construct() {
+		$this->api_baseurl = "http://api.joinbrink.com/v1/";
+	}
+
+	private function send($args) {
+		
+		$custom_request = false;
+		$data = $http_header = array();
+		extract($args);
+		
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+		if ($custom_request) curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $custom_request);
+
+		if ($data) curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+		if ($http_header) curl_setopt($ch, CURLOPT_HTTPHEADER, $http_header);
+		
+		$response = curl_exec($ch);
+		curl_close($ch);
+		
+		return $response;
+	}
+	
+	public function create_user($data) {
+		
+		// $data (array) attributes:
+		// first_name,
+		// last_name,
+		// email,
+		// username,
+		// password
+		
+		$args = array(
+			'url' => $this->api_baseurl.'users',
+			'custom_request' => 'PUT',
+			'data' => $data,
+			'http_header' => array("Content-Type: application/json")
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	}
+	
+	public function get_user() {
+		
+		$args = array(
+			'url' => $this->api_baseurl."users/".$this->user_id,
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+		
+	}
+	
+	public function login($data) {
+		
+		// $data (array) attributes:
+		// username,
+		// password
+
+		$args = array(
+			'url' => $this->api_baseurl."login",
+			'custom_request' => 'POST',
+			'data' => $data,
+			'http_header' => array(
+				"Content-Type: application/json"
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	
+	}
+	
+	public function get_all_flights() {
+
+		$args = array(
+			'url' => $this->api_baseurl."flights",
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	}
+	
+	public function get_flight($data) {
+		
+		// $data (array) attributes:
+		// flight_id
+
+		$args = array(
+			'url' => $this->api_baseurl."flights/".$data['flight_id'],
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+		
+	}
+	
+	public function create_flight() {
+
+		$args = array(
+			'url' => $this->api_baseurl."flights",
+			'custom_request' => 'PUT',
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	}
+	
+	public function get_flight_data($data) {
+		
+		// $data (array) attributes:
+		// flight_id,
+		// prop (array) attributes:
+			// page,
+			// per_page
+
+		$args = array(
+			'url' => $this->api_baseurl."flights/".$data['flight_id']."/data",
+			'data' => $data['prop'],
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	}
+	
+	public function create_flight_data_record($data) {
+
+		// $data (array) attributes:
+		// flight_id,
+		// prop (array) attributes:
+			// timestamp,
+			// altitude,
+			// barometricPressure,
+			// coordinateX,
+			// coordinateY,
+			// temperature
+
+		$args = array(
+			'url' => $this->api_baseurl."flights/".$data['flight_id']."/data",
+			'custom_request' => 'PUT',
+			'data' => $data['prop'],
+			'http_header' => array(
+				"Content-Type: application/json",
+				"Authorization: JWT ".$this->access_token
+			)
+		);
+		$response = $this->send($args);
+		
+		return json_decode($response);
+	
+	}
+	
+}
